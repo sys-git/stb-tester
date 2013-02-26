b31d065c81ca7faa248e744c91cb4cc8cf9ad3b1
sys-git <francis.horsman@gmail.com>
2013-02-26 14:31:30 +0000

    Update to Trunk - Irnetbox update

--- StbTester/remotes/impls/irnetbox/Irnetbox.py
+++ StbTester/remotes/impls/irnetbox/Irnetbox.py
@@ -48,8 +48,8 @@ import sys
 import time
 
 class IRNetBox(object):
-    def __init__(self, hostname, debugger):
-        port = 10001  # §5
+    def __init__(self, hostname):
+        port = 10001 # §5
         for i in range(6):
             try:
                 self._socket = socket.socket()
@@ -66,6 +66,8 @@ class IRNetBox(object):
                 else:
                     raise
         self._responses = _read_responses(self._socket)
+        self.irnetbox_model = 0
+        self._get_version()
 
     def __enter__(self):
         return self
@@ -76,21 +78,27 @@ class IRNetBox(object):
     def power_on(self):
         """Power on the irNetBox device (§5.2.3).
 
-        §5.2.3 calls this "power to the CPLD device"; the irNetBox-III doesn't
-        have a CPLD, but according to Chris Dodge @ RedRat, this is now "power
-        on the whole irNetBox".
+§5.2.3 calls this "power to the CPLD device"; the irNetBox-III doesn't
+have a CPLD, but according to Chris Dodge @ RedRat, this is now "power
+on the whole irNetBox".
 
-        """
+"""
         self._send(MessageTypes.POWER_ON)
 
     def power_off(self):
         """Put the irNetBox in low-power standby mode (§5.2.3).
 
-        In low power mode the LEDs on the front will be doing the Cylon
-        pattern.
+In low power mode the LEDs on the front will be doing the Cylon
+pattern.
 
-        """
+"""
         self._send(MessageTypes.POWER_OFF)
+
+    def reset(self):
+        """Reset the CPLD"""
+        self._send(
+            MessageTypes.CPLD_INSTRUCTION,
+            struct.pack("B", 0x00))
 
     def indicators_on(self):
         """Enable LED indicators on the front panel (§5.2.4)."""
@@ -107,30 +115,61 @@ class IRNetBox(object):
     def irsend_raw(self, port, power, data):
         """Output the IR data on the given port at the set power (§6.1.1).
 
-        * `port` is a number between 1 and 16.
-        * `power` is a number between 1 and 100.
-        * `data` is a byte array as exported by the RedRat Signal DB Utility.
+* `port` is a number between 1 and 16.
+* `power` is a number between 1 and 100.
+* `data` is a byte array as exported by the RedRat Signal DB Utility.
 
-        """
-        ports = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ports[port-1] = power
-        sequence_number = random.randint(0, 2**16 - 1)
-        delay = 0  # use the default delay of 100ms
-        self._send(
-            MessageTypes.OUTPUT_IR_ASYNC,
-            struct.pack(
-                ">HH16s%ds" % len(data),
-                sequence_number,
-                delay,
-                struct.pack("16B", *ports),
-                data))
+"""
+        if self.irnetbox_model == NetBoxTypes.MK1:
+            raise Exception("IRNetBox MK1 not supported")
+        elif self.irnetbox_model == NetBoxTypes.MK2:
+            self.reset()
+            self.indicators_on()
+            self._send(MessageTypes.SET_MEMORY)
+            self._send(MessageTypes.CPLD_INSTRUCTION, struct.pack("B", 0x00))
+            if power < 33:
+                self._send(
+                    MessageTypes.CPLD_INSTRUCTION,
+                    struct.pack("B", port + 1))
+            elif power < 66:
+                self._send(
+                    MessageTypes.CPLD_INSTRUCTION,
+                    struct.pack("B", port + 31))
+            else:
+                self._send(
+                    MessageTypes.CPLD_INSTRUCTION,
+                    struct.pack("B", port + 1))
+                self._send(
+                    MessageTypes.CPLD_INSTRUCTION,
+                    struct.pack("B", port + 31))
+            self._send(MessageTypes.DOWNLOAD_SIGNAL, data)
+            self._send(MessageTypes.OUTPUT_IR_SIGNAL)
+            self.reset()
+        else:
+            ports = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ports[port-1] = power
+            sequence_number = random.randint(0, 2**16 - 1)
+            delay = 0 # use the default delay of 100ms
+            self._send(
+                MessageTypes.OUTPUT_IR_ASYNC,
+                struct.pack(
+                    ">HH16s%ds" % len(data),
+                    sequence_number,
+                    delay,
+                    struct.pack("16B", *ports),
+                    data))
 
     def _send(self, message_type, message_data=""):
         self._socket.sendall(_message(message_type, message_data))
-        response_type, _response_data = self._responses.next()
+        response_type, response_data = self._responses.next()
         if response_type == MessageTypes.ERROR:
             raise Exception("IRNetBox returned ERROR")
+        if response_type == MessageTypes.DEVICE_VERSION:
+            self.irnetbox_model, = struct.unpack(
+                '<H', response_data[10:12]) # == §5.2.6's payload_data[8:10]
 
+    def _get_version(self):
+        self._send(MessageTypes.DEVICE_VERSION)
 
 class RemoteControlConfig:
     def __init__(self, filename):
@@ -139,13 +178,16 @@ class RemoteControlConfig:
     def __getitem__(self, key):
         return self._data[key]
 
-
-class MessageTypes:
+class MessageTypes(object):
     """§5.2"""
     ERROR = 0x01
     POWER_ON = 0x05
     POWER_OFF = 0x06
     CPLD_INSTRUCTION = 0x07
+    DEVICE_VERSION = 0x09
+    SET_MEMORY = 0x10
+    DOWNLOAD_SIGNAL = 0x11
+    OUTPUT_IR_SIGNAL = 0x12
     OUTPUT_IR_ASYNC = 0x30
     IR_ASYNC_COMPLETE = 0x31
 
@@ -159,15 +201,20 @@ MESSAGE_NAMES = {
     MessageTypes.IR_ASYNC_COMPLETE: "IR_ASYNC_COMPLETE",
 }
 
+class NetBoxTypes(object):
+    """§5.2.6"""
+    MK1 = 2
+    MK2 = 7
+    MK3 = 8
 
 def _message(message_type, message_data):
     # §5.1. Message Structure: Host to irNetBox
     #
-    # '#'              byte     The '#' character indicates to the control
-    #                           microprocessor the start of a message.
-    # Message length   ushort   The length of the data section of this message.
-    # Message type     byte     One of the values listed below.
-    # Data             byte[]   Any data associated with this type of message.
+    # '#' byte The '#' character indicates to the control
+    # microprocessor the start of a message.
+    # Message length ushort The length of the data section of this message.
+    # Message type byte One of the values listed below.
+    # Data byte[] Any data associated with this type of message.
     #
     # A ushort value is a 16-bit unsigned integer in big-endian format.
     #
@@ -178,18 +225,17 @@ def _message(message_type, message_data):
         message_type,
         message_data)
 
-
 def _read_responses(stream):
     """Generator that splits stream into (type, data) tuples."""
 
     # §5.1. Message Structure: irNetBox to Host
     #
-    # Message length   ushort   The length of the data section of this message.
-    # Message type     byte     Contains either:
-    #                           a) The same value as the original message from
-    #                              the host, or
-    #                           b) A value (0x01) indicating "Error".
-    # Data             byte[]   Any data associated with this type of message.
+    # Message length ushort The length of the data section of this message.
+    # Message type byte Contains either:
+    # a) The same value as the original message from
+    # the host, or
+    # b) A value (0x01) indicating "Error".
+    # Data byte[] Any data associated with this type of message.
     #
     buf = ""
     while True:
@@ -208,26 +254,22 @@ def _read_responses(stream):
                 yield response_type, response_data
             buf = buf[3+data_len :]
 
-
-def _parse_config(file):
-    """Read irNetBox configuration file.
-
+def _parse_config(file_):
+    """
+    Read irNetBox configuration file.
     Which is produced by RedRat's (Windows-only) "IR Signal Database Utility".
-
     This doesn't support config files with "double signals" (where 2 different
     signals were recorded from alternate presses of the same button on the
     remote control unit).
-
     """
     d = {}
-    for line in file:
+    for line in file_:
         fields = re.split("[\t ]+", line.rstrip(), maxsplit=3)
         if len(fields) == 4:
             name, type_, max_num_lengths, data = fields
             if type_ == "MOD_SIG":
                 d[name] = binascii.unhexlify(data)
     return d
-
 
 # Tests
 #===========================================================================
@@ -258,12 +300,12 @@ def test_that_parse_config_understands_redrat_format():
     f = StringIO.StringIO(re.sub("^ +", "", flags=re.MULTILINE, string=
         """Device TestRCU
 
-        Note: The data is of the form <signal name> MOD_SIG <max_num_lengths> <byte_array_in_ascii_hex>.
+Note: The data is of the form <signal name> MOD_SIG <max_num_lengths> <byte_array_in_ascii_hex>.
 
-        DOWN    MOD_SIG    16 000174F5FF60000000060000004802450222F704540D12116A464F0000000000000000000000000000000000000000000102020202020202020202020202020202020202020202020202030202020202020202020202030202020202020203020202030203020202030203020302020203027F0004027F
+DOWN MOD_SIG 16 000174F5FF60000000060000004802450222F704540D12116A464F0000000000000000000000000000000000000000000102020202020202020202020202020202020202020202020202030202020202020202020202030202020202020203020202030203020202030203020302020203027F0004027F
 
-        UP    MOD_SIG    16 000174FAFF60000000050000004803457422F7045A0D13116A00000000000000000000000000000000000000000000000102020202020202020202020202020202020202020202020202030202020202020203020202020202020202020203020202020203020302030203020302020203027F0004027F
-        """))
+UP MOD_SIG 16 000174FAFF60000000050000004803457422F7045A0D13116A00000000000000000000000000000000000000000000000102020202020202020202020202020202020202020202020202030202020202020203020202020202020202020203020202020203020302030203020302020203027F0004027F
+"""))
     config = _parse_config(f)
     assert config["DOWN"].startswith("\x00\x01\x74\xF5")
     assert config["UP"].startswith("\x00\x01\x74\xFA")
@@ -272,15 +314,15 @@ def test_that_parse_config_understands_redrat_format():
 class _FileToSocket:
     """Makes something File-like behave like a Socket for testing purposes.
 
-    >>> import StringIO
-    >>> s = _FileToSocket(StringIO.StringIO('Hello'))
-    >>> s.recv(3)
-    'Hel'
-    >>> s.recv(3)
-    'lo'
-    >>> s.recv(3)
-    ''
-    """
+>>> import StringIO
+>>> s = _FileToSocket(StringIO.StringIO('Hello'))
+>>> s.recv(3)
+'Hel'
+>>> s.recv(3)
+'lo'
+>>> s.recv(3)
+''
+"""
     def __init__(self, f):
         self.file = f
 
