2196da3328de10d255e3eea6a3c11b21f2ddf474
sys-git <francis.horsman@gmail.com>
2013-03-11 14:16:44 +0000

    Added timestamp to AbortedException output

--- StbTester/apis/impls/common/errors/AbortedException.py
+++ StbTester/apis/impls/common/errors/AbortedException.py
@@ -5,7 +5,8 @@ Created on 8 Nov 2012
 '''
 
 from StbTester.apis.impls.common.errors.UITestError import UITestError
+from StbTester.core.utils.TimeUtils import timestamp
 
 class AbortedException(UITestError):
     def __str__(self):
-        return "Aborted @ %(M)s"%{"M":self.message}
+        return "Aborted @ %(M)s"%{"M":timestamp(self.message)}
